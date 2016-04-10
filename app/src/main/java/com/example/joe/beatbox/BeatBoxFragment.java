8e3b65fbff2d9854523c17a53b8b31314fd3b6d0
Joe Stevens <joseph.stevens34@gmail.com>
2016-04-10 16:59:12 -0400

    Wiring Up Assets for Use

--- app/src/main/java/com/example/joe/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/example/joe/beatbox/BeatBoxFragment.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.List;
+
 /**
  * Created by Joe on 4/5/2016.
  */
@@ -45,15 +47,27 @@ public class BeatBoxFragment extends Fragment {
 
     private class SoundHolder extends RecyclerView.ViewHolder{
         private Button mButton;
+        private Sound mSound;
 
         public SoundHolder(LayoutInflater inflater, ViewGroup container){
             super(inflater.inflate(R.layout.list_item_sound, container, false));
 
             mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
         }
+
+        public void bindSound(Sound sound){
+            mSound = sound;
+            mButton.setText(mSound.getName());
+        }
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds){
+            mSounds = sounds;
+        }
 
         @Override
         public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -63,8 +77,9 @@ public class BeatBoxFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(SoundHolder holder, int position) {
-
+        public void onBindViewHolder(SoundHolder soundHolder, int position) {
+            Sound sound = mSounds.get(position);
+            soundHolder.bindSound(sound);
         }
 
         @Override
---
+++ app/src/main/java/com/example/joe/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package com.example.joe.beatbox;
+
+/**
+ * Created by Joe on 4/10/2016.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
